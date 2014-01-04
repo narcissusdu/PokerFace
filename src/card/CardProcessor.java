4eaea597c2f3d85e962adfb44be73115f2273607
Narcissus Du <narcissusdu@gmail.com>
2014-01-05 00:46:30 +0800

    build 7-card instance from 5 river cards while excluding 2 known cards

--- src/card/CardProcessor.java
+++ src/card/CardProcessor.java
@@ -3,13 +3,14 @@ package card;
 import java.util.ArrayList;
 
 public class CardProcessor {
-	
+	public static int i = 0;
 	public void Process(ArrayList<Card> cardList){
-		System.out.println("------------------------------------");
 		for(int i =0;i<cardList.size();i++){
 			System.out.print(i);
 			cardList.get(i).showCard();
-		}
+		}		
+		System.out.println(++i);
+		System.out.println();
 		
 	};
 }
--- src/card/FullCardBuilder.java
+++ src/card/FullCardBuilder.java
@@ -1,14 +1,23 @@
 package card;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import MyException.CardInitialException;
 
 public class FullCardBuilder {
 	public FullCardBuilder(){}
 	
-	public final int maxCards = 7;
+	//public final int maxCards = 7;
 	
-	public void ProcessCards(Card[] riverCards,Card[] knownCards){
+	public void ProcessCards(Card[] riverCards,Card[] knownCards) throws CardInitialException{
 		ArrayList<Integer> intList = new ArrayList<Integer>();
+		Card[] targetCards= new Card[7];
+		ArrayList<Card> cardList = new ArrayList<Card>(Arrays.asList(riverCards));
+		cardList.add(null);
+		cardList.add(null);
+		
+		CardSelector cs = new CardSelector(); 
 		
 		for(int i=0;i<riverCards.length;i++)
 			intList.add(riverCards[i].getCardIndex());			
@@ -19,11 +28,12 @@ public class FullCardBuilder {
 		for(int i=1;i<52;i++){
 			if(intList.contains(i))
 				continue;
+			cardList.set(5, new Card(i));
 			for(int j=i+1;j<53;j++){
 				if(intList.contains(j))
 					continue;
-				
-				
+				cardList.set(6, new Card(j));
+				cs.SelectCard(cardList.toArray(targetCards), 5);
 			}			
 		}
 	}
