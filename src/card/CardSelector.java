5e750ad943665e3b73511b8c725d760414c72797
Narcissus Du <narcissusdu@gmail.com>
2014-01-05 00:29:27 +0800

    added some comment

--- src/card/CardSelector.java
+++ src/card/CardSelector.java
@@ -3,35 +3,55 @@ package card;
 import java.util.ArrayList;
 
 public class CardSelector {
-	public CardSelector(){}
+	public CardSelector() {
+	}
+
 	public void SelectCard(Card[] cards, int count) {
+		
 		if (count > cards.length)
 			return;
-		// Card[] resultCards = new Card[count];
+		
+		//simulation of the pointers.
 		int[] pointers = new int[count];
+		
+		//set up the initial state of the pointers
+		//make sure the pointers are working in increasing order
 		for (int tmp = 0; tmp < count; tmp++)
 			pointers[tmp] = tmp;
-
+		
 		int workPointer = count - 1;
+		
 		do {
+			
+			//the sequence of the elements must not be changed in this method 
 			Process(cards, pointers);
+			
+			//save the state of the work-pointer in order to notice that 
+			//it has been move left after the calculation
 			int lastPointer = workPointer;
-			workPointer = getcurrentWorkPointer(cards.length,pointers);
+			
+			//find out which pointer should be moved left 
+			workPointer = getcurrentWorkPointer(cards.length, pointers);
 			pointers[workPointer]++;
-			if(lastPointer>workPointer)
-				for(int i = workPointer+1;i<count;i++)
-					pointers[i]=pointers[i-1]+1;			
+			
+			//reset the positions of pointers right to the work-pointer
+			if (lastPointer > workPointer)
+				for (int i = workPointer + 1; i < count; i++)
+					pointers[i] = pointers[i - 1] + 1;
 		} while (pointers[0] != cards.length - count);
+		
+		//
 		Process(cards, pointers);
 		// return resultCards;
 	}
 
 	private int getcurrentWorkPointer(int arrayLength, int[] pointers) {
-		int workPointer =pointers.length-1 ;
-		if(pointers[workPointer] < arrayLength-1)
+		int workPointer = pointers.length - 1;
+		if (pointers[workPointer] < arrayLength - 1)
 			return workPointer;
-		for(;pointers[workPointer-1]==pointers[workPointer]-1;workPointer--);
-		return workPointer-1;
+		for (; pointers[workPointer - 1] == pointers[workPointer] - 1; workPointer--)
+			;
+		return workPointer - 1;
 	}
 
 	public void Process(Card[] cards, int[] pointers) {
--- src/card/FullCardBuilder.java
+++ src/card/FullCardBuilder.java
@@ -5,7 +5,26 @@ import java.util.ArrayList;
 public class FullCardBuilder {
 	public FullCardBuilder(){}
 	
-	public void ProcessCards(Card[] cards,Card[] knownCards,int needCount){
+	public final int maxCards = 7;
+	
+	public void ProcessCards(Card[] riverCards,Card[] knownCards){
+		ArrayList<Integer> intList = new ArrayList<Integer>();
 		
+		for(int i=0;i<riverCards.length;i++)
+			intList.add(riverCards[i].getCardIndex());			
+		
+		for(int i=0;i<knownCards.length;i++)
+			intList.add(knownCards[i].getCardIndex());
+		
+		for(int i=1;i<52;i++){
+			if(intList.contains(i))
+				continue;
+			for(int j=i+1;j<53;j++){
+				if(intList.contains(j))
+					continue;
+				
+				
+			}			
+		}
 	}
 }
